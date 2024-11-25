c1d12add031f6cd3bdabb62bbbe61f1a6888019d
HGX_GEO <2307864143@qq.com>
2024-11-25 21:59:10 +0800

    添加可视域分析

--- src/components/analysis/VisualArea.vue
+++ src/components/analysis/VisualArea.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
     <div class="button-group">
-      <button class="common-btn" @click="setvisible('add')">添加可视域</button>
-      <button class="common-btn" @click="setvisible('remove')">删除可视域</button>
+      <button class="common-btn" @click="addVisualAnalysis">添加可视域</button>
+      <button class="common-btn" @click="removeVisualLayer">删除可视域</button>
     </div>
   </div>
 </template>
@@ -13,37 +13,25 @@ import { ViewerStore } from "@/store";
 
 const viewerStore = ViewerStore();
 const viewer = viewerStore.viewer;
-// 开关
-function setvisible(value) {
-    switch (value) {
-      case 'add':
-        addViewField();
-        break;
-      case 'remove':
-        clearAllViewField();
-        break;
-    }
-}
-var arrViewField = [];
-var viewModel = { verticalAngle: 90, horizontalAngle: 120, distance: 10 };
-function addViewField() {
-    var e = new Cesium.ViewShed3D(viewer, {
-      horizontalAngle: Number(viewModel.horizontalAngle),
-      verticalAngle: Number(viewModel.verticalAngle),
-      distance: Number(viewModel.distance),
-      calback: function () {
-        viewModel.distance = e.distance
-      }
-    });
-    arrViewField.push(e);
-};
 
-function clearAllViewField() {
-  for (var e = 0, i = arrViewField.length; e < i; e++){
-    arrViewField[e].destroy();
+let viewshedValue = ""
+//开始可视分析
+function addVisualAnalysis(){
+  var viewsheds = [];
+  var options = {
+      qdOffset: 2,
+      zdOffset: 2
   };
-  arrViewField = [];
-};
+  viewshedValue = new viewshed(viewer, options);
+  viewsheds.push(viewshed);
+}
+
+//移除可视化分析
+function removeVisualLayer(){
+  if(viewshedValue){
+     viewshedValue.clear();
+  };
+}
 
 //添加三位模型
 async function set3Dtitle() {
@@ -101,170 +89,10 @@ async function set3Dtitle() {
   });
 };
 
-const createScriptDom = () => {
-  // 创建 <script> 元素
-  const script = document.createElement("script");
-  script.id = "fragment-shader";
-  script.type = "notjs";
-  script.text = `
-      uniform float czzj;
-      uniform float dis;
-      uniform float spzj;
-      uniform vec3 visibleColor;
-      uniform vec3 disVisibleColor;
-      uniform float mixNum;
-      uniform sampler2D colorTexture;
-      uniform sampler2D stcshadow; 
-      uniform sampler2D depthTexture;
-      uniform mat4 shadowMap_matrix; 
-      uniform vec4 shadowMap_lightPositionEC; 
-      uniform vec4 shadowMap_lightDirectionEC;
-      uniform vec3 shadowMap_lightUp;
-      uniform vec3 shadowMap_lightDir;
-      uniform vec3 shadowMap_lightRight;
-      uniform vec4 shadowMap_normalOffsetScaleDistanceMaxDistanceAndDarkness; 
-      uniform vec4 shadowMap_texelSizeDepthBiasAndNormalShadingSmooth; 
-      in vec2 v_textureCoordinates;  // 使用in替换varying
-
-      out vec4 fragColor; // 使用out替换gl_FragColor
-
-      // 将toEye函数的返回值更改为vec4，保持正确的类型
-      vec4 toEye(in vec2 uv, in float depth){
-          vec2 xy = vec2((uv.x * 2.0 - 1.0), (uv.y * 2.0 - 1.0));
-          vec4 posInCamera = czm_inverseProjection * vec4(xy, depth, 1.0);
-          posInCamera /= posInCamera.w;
-          return posInCamera;
-      }
-
-      float getDepth(in vec4 depth){
-          float z_window = czm_unpackDepth(depth);
-          return czm_reverseLogDepth(z_window);
-      }
-
-      float sampleShadowMap(sampler2D shadowMap, vec2 uv){
-          return texture(shadowMap, uv).r;  // 使用texture替代texture2D
-      }
-
-      float shadowDepthCompare(sampler2D shadowMap, vec2 uv, float depth){
-          return step(depth, sampleShadowMap(shadowMap, uv));
-      }
-
-      float shadowVisibility(sampler2D shadowMap, czm_shadowParameters shadowParameters){
-          float depthBias = shadowParameters.depthBias;
-          float depth = shadowParameters.depth;
-          float nDotL = shadowParameters.nDotL;
-          float normalShadingSmooth = shadowParameters.normalShadingSmooth;
-          float darkness = shadowParameters.darkness;
-          vec2 uv = shadowParameters.texCoords;
-          depth -= depthBias;
-          vec2 texelStepSize = shadowParameters.texelStepSize;
-          float radius = 1.0;
-
-          float visibility = 
-              (shadowDepthCompare(shadowMap, uv, depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(-texelStepSize.x, -texelStepSize.y), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(0.0, -texelStepSize.y), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(texelStepSize.x, -texelStepSize.y), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(-texelStepSize.x, 0.0), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(texelStepSize.x, 0.0), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(-texelStepSize.x, texelStepSize.y), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(0.0, texelStepSize.y), depth) +
-              shadowDepthCompare(shadowMap, uv + vec2(texelStepSize.x, texelStepSize.y), depth)) * (1.0 / 9.0);
-
-          return visibility;
-      }
-
-      vec3 projectPointOnPlane(in vec3 planeNormal, in vec3 planeOrigin, in vec3 point){
-          vec3 v01 = point - planeOrigin;
-          float d = dot(planeNormal, v01);
-          return (point - planeNormal * d);
-      }
-
-      float calculateDistance(vec3 pt){
-          return sqrt(pt.x * pt.x + pt.y * pt.y + pt.z * pt.z);
-      }
-
-      void main() { 
-          const float PI = 3.141592653589793;
-          vec4 color = texture(colorTexture, v_textureCoordinates);
-          vec4 currDepth = texture(depthTexture, v_textureCoordinates);
-
-          if(currDepth.r >= 1.0){
-              fragColor = color;  // 使用fragColor替换gl_FragColor
-              return;
-          }
-          
-          float depth = getDepth(currDepth);
-          vec4 positionEC = toEye(v_textureCoordinates, depth);
-          vec3 normalEC = vec3(1.0);
-
-          czm_shadowParameters shadowParameters; 
-          shadowParameters.texelStepSize = shadowMap_texelSizeDepthBiasAndNormalShadingSmooth.xy; 
-          shadowParameters.depthBias = shadowMap_texelSizeDepthBiasAndNormalShadingSmooth.z; 
-          shadowParameters.normalShadingSmooth = shadowMap_texelSizeDepthBiasAndNormalShadingSmooth.w; 
-          shadowParameters.darkness = shadowMap_normalOffsetScaleDistanceMaxDistanceAndDarkness.w; 
-          shadowParameters.depthBias *= max(depth * 0.01, 1.0); 
-
-          vec3 directionEC = normalize(positionEC.xyz - shadowMap_lightPositionEC.xyz); 
-          float nDotL = clamp(dot(normalEC, -directionEC), 0.0, 1.0); 
-
-          vec4 shadowPosition = shadowMap_matrix * positionEC; 
-          shadowPosition /= shadowPosition.w; 
-
-          if (any(lessThan(shadowPosition.xyz, vec3(0.0))) || any(greaterThan(shadowPosition.xyz, vec3(1.0)))) { 
-              fragColor = color;
-              return;
-          };
-            
-          vec4 lw = czm_inverseView * vec4(shadowMap_lightPositionEC.xyz, 1.0);
-          vec4 vw = czm_inverseView * vec4(positionEC.xyz, 1.0);
-
-          if(distance(lw.xyz, vw.xyz) > dis){
-              fragColor = color;
-              return;
-          }
-
-          vec3 ptOnSP = projectPointOnPlane(shadowMap_lightUp, lw.xyz, vw.xyz);
-          directionEC = ptOnSP - lw.xyz;
-          float degJJ = acos(dot(directionEC, shadowMap_lightDir) / (calculateDistance(directionEC) * calculateDistance(shadowMap_lightDir))) * (180.0 / PI);
-
-          if(abs(degJJ) > spzj / 2.0){
-              fragColor = color;
-              return;
-          }
-
-          vec3 ptOnCZ = projectPointOnPlane(shadowMap_lightRight, lw.xyz, vw.xyz);
-          vec3 dirOnCZ = ptOnCZ - lw.xyz;
-          float degJJCZ = acos(dot(dirOnCZ, shadowMap_lightDir) / (calculateDistance(dirOnCZ) * calculateDistance(shadowMap_lightDir))) * (180.0 / PI);
-
-          if(abs(degJJCZ) > czzj / 2.0){
-              fragColor = color;
-              return;
-          }
-
-          shadowParameters.texCoords = shadowPosition.xy; 
-          shadowParameters.depth = shadowPosition.z; 
-          shadowParameters.nDotL = nDotL; 
-          float visibility = shadowVisibility(stcshadow, shadowParameters); 
-
-          if(visibility == 1.0){
-              fragColor = mix(color, vec4(visibleColor, 1.0), mixNum);
-          } else {
-              if(abs(shadowPosition.z - 0.0) < 0.01){
-                  return;
-              }
-              fragColor = mix(color, vec4(disVisibleColor, 1.0), mixNum);
-          }
-      }
-    `;
-  // 将 <script> 标签添加到 DOM（可以是 document.body 或其他适当的父容器）
-  document.body.appendChild(script);
-}
 
 onMounted(() => {
   viewer.scene.globe.depthTestAgainstTerrain = true;
   set3Dtitle();
-  createScriptDom()
 });
 
 onUnmounted(() => {
